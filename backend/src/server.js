387cbb35a998c7ba819e6980ce7b7fc307fec367
septiaaa <Septianapujiastuti15@gmail.com>
2026-01-19 13:40:19 +0700

    Update server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,12 @@ db.getConnection()
 /* =====================
    MIDDLEWARE
 ===================== */
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
